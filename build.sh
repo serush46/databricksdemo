7fddf83e08b2cebfde9215d969dced00d413f3d9
Suresh kumar <skumar7@vmware.com>
2022-07-25 17:47:19 +0530

    ECR repo

--- build.sh
+++ build.sh
@@ -12,13 +12,10 @@ fi
 
 docker build -f Dockerfile . -t app-release-$version
 
-export ECR_URL=xxxxx.dkr.ecr.us-west-2.amazonaws.com
-export AWS_ACCESS_KEY_ID=$(aws configure get wavefront-dev.aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(aws configure get wavefront-dev.aws_secret_access_key)
+export ECR_URL=095415062695.dkr.ecr.us-west-2.amazonaws.com
+
 
 [[ -z "$ECR_URL" ]] && { echo "ECR_URL is empty" ; exit 1; }
-[[ -z "$AWS_ACCESS_KEY_ID" ]] && { echo "AWS_ACCESS_KEY_ID is empty" ; exit 1; }
-[[ -z "$AWS_SECRET_ACCESS_KEY" ]] && { echo "AWS_SECRET_ACCESS_KEY is empty" ; exit 1; }
 
 docker tag "app-release-$version" "${ECR_URL}/app-release:$version"
 $(aws ecr get-login --no-include-email --region us-west-2)
